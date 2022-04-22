c212c2a9bf2a895aa587773e0e675427c9cb1742
pranv11 <87796245+pranv11@users.noreply.github.com>
2022-04-21 19:18:06 -0700

    Add files via upload

--- send.py
+++ send.py
@@ -1,15 +1,18 @@
-# importing the module
 from selenium import webdriver
 import csv
 from selenium.webdriver.common.keys import Keys
 import time
 
-
-driver = webdriver.Chrome(executable_path='whatsapp script\chromedriver.exe')
+#asking input from user.
+location_of_chromedriver = input("enter the path of chromedriver: ")
 file_numbers = input("enter the name of the files with numbers: ")
+msg = input("enter the message you want to send: ")
+
+driver = webdriver.Chrome(executable_path= location_of_chromedriver)
+
+
 file = open(file_numbers, "r")
 csv_reader = csv.reader(file)
-
 lists_from_csv = []
 string_message= ""
 numbers_from_csv = []
@@ -17,20 +20,20 @@ var1_from_csv = []
 var2_from_csv = []
 var3_from_csv = []
 
-
+#assigning variables to arrays
 for row in csv_reader:
     numbers_from_csv.append(row[0])
     var1_from_csv.append(row[1])
     var2_from_csv.append(row[2])
     var3_from_csv.append(row[3])
 
+#opening whatsapp
 driver.get('https://web.whatsapp.com/')
-
-msg = input("enter the message you want to send: ")
 
 
 input('Enter anything after scanning QR code')
 
+#looping to change the variables according to the data provided.
 for i in range(len(numbers_from_csv)):
     with open(msg) as f:
         message = f.read()
