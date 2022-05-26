01c7a61e49171a2d30910af3f7744acd4a0487b1
pranav pandey <87796245+pranv11@users.noreply.github.com>
2022-05-26 15:40:36 -0700

    Update send.py

--- send.py
+++ send.py
@@ -3,13 +3,13 @@ import csv
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 #asking input from user.
 location_of_chromedriver = input("enter the path of chromedriver: ")
 file_numbers = input("enter the name of the files with numbers: ")
 msg = input("enter the message you want to send: ")
 
 driver = webdriver.Chrome(executable_path= location_of_chromedriver)
-
 
 file = open(file_numbers, "r")
 csv_reader = csv.reader(file)
@@ -21,17 +21,21 @@ var2_from_csv = []
 var3_from_csv = []
 
 #assigning variables to arrays
-for row in csv_reader:
-    numbers_from_csv.append(row[0])
-    var1_from_csv.append(row[1])
-    var2_from_csv.append(row[2])
-    var3_from_csv.append(row[3])
+try:
+    for row in csv_reader:
+        numbers_from_csv.append(row[0])
+        var1_from_csv.append(row[1])
+        var2_from_csv.append(row[2])
+        var3_from_csv.append(row[3])
+except:
+    print("the csv file input is invalid")
 
 #opening whatsapp
 driver.get('https://web.whatsapp.com/')
 
 
-input('Enter anything after scanning QR code')
+input('press enter after scanning QR code')
+
 
 #looping to change the variables according to the data provided.
 for i in range(len(numbers_from_csv)):
@@ -42,17 +46,20 @@ for i in range(len(numbers_from_csv)):
     z = y.replace("x3", var3_from_csv[i])
 
 
+    try:
 
-    user = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div/div[2]')
+        user = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/div[2]/div/div[2]')
 
-    user.send_keys(numbers_from_csv[i])
-    user.send_keys(Keys.RETURN)
+        user.send_keys(numbers_from_csv[i])
+        user.send_keys(Keys.RETURN)
 
-    msg_box = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
+        msg_box = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
 
 
-    msg_box.send_keys(z)
-    time.sleep(1)
+        msg_box.send_keys(z)
+        time.sleep(1)
 
-    msg_box.send_keys(Keys.RETURN)
-        
+        msg_box.send_keys(Keys.RETURN)
+    except:
+        print(f"An error occurred in sending message to {i}")
+            
